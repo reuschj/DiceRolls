fe9039c7bcbd5e58a56eafa72d451b8d92ee31a9
reuschj <reuschj@gmail.com>
2018-03-27 22:12:12 -0500

    Initial Commit

---
+++ DiceRolls/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DiceRolls
+//
+//  Created by Justin Reusch on 3/27/18.
+//  Copyright © 2018 Justin Reusch. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DiceRolls/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DiceRolls
+//
+//  Created by Justin Reusch on 3/27/18.
+//  Copyright © 2018 Justin Reusch. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
